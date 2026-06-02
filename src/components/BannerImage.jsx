a025247c2612ab1014acd39c68afce1543d58e25
mithileshmaddhesia <mithleshmaddeshiya3@gmail.com>
2026-06-02 22:41:32 +0530

    add new more things

--- src/components/BannerImage.jsx
+++ src/components/BannerImage.jsx
@@ -6,7 +6,7 @@ export default function BannerImage({ data }) {
 
   
   return (
-    <section className="py-3">
+    <section className="py-2">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
         <div className="relative w-full h-[180px] md:h-[280px] rounded-2xl overflow-hidden shadow-sm">
